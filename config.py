b11b8275e4b52841d979c06c7af529f41726c732
Martynas Balaišis <martynas.balaisis@teliacompany.com>
2022-12-01 11:07:12 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Get current directory
+base_path = os.path.dirname(__file__)
+
+# Get path to inputs directory
+inputs_path = os.path.join(base_path, 'inputs')
+
---
+++ day01.py
@@ -0,0 +1,23 @@
+# input - number of Calories each Elf is carrying: https://adventofcode.com/2022/day/1/input
+from config import *
+
+inputs_file = os.path.join(inputs_path, 'day01.txt')
+
+# Find max calories carried by single Elf
+top_3_calories = [0, 0, 0]
+this_elf_calories = 0
+
+with open(inputs_file, 'r') as inputs:
+    for line in inputs:
+        # If line is blank - check calorie max
+        if len(line.strip()) < 1:
+            if this_elf_calories > min(top_3_calories):
+                # replace smallest number in the list
+                index = top_3_calories.index(min(top_3_calories))
+                top_3_calories[index] = this_elf_calories
+            this_elf_calories = 0
+        else:
+            this_elf_calories += int(line)
+
+    print(f'Most callories carried by singe dude: {max(top_3_calories)}')
+    print(f'Callories carried by top 3 elfs: {sum(top_3_calories)}')
