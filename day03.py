1a7db1f4de98a9a4bb83c4f9716addcceaf1ad03
Marchyus <martynasb@gmail.com>
2022-12-03 08:05:49 +0200

    slightly cleaned up

--- day03.py
+++ day03.py
@@ -2,55 +2,42 @@ from config import *
 
 inputs_file = os.path.join(inputs_path, 'day03.txt')
 
+# Build list of rucksacks
 rucksacks = []
 with open(inputs_file, 'r') as inputs:
     for line in inputs:
         rucksacks.append(line.strip())
 
 
-priority = list(string.ascii_letters)
+# function to calculate sum of priorities
+def calculate_prio(item_list):
+    priority = list(string.ascii_letters)
+    sum_of_priorities = 0
+    for item in item_list:
+        sum_of_priorities += (priority.index(item) + 1)
+    return sum_of_priorities
 
 
-
-matching_letters = []
+# find matching item in every single backpack
+matching_items = []
 for rucksack in rucksacks:
     first_half, second_half = rucksack[0:len(rucksack)//2], rucksack[len(rucksack)//2:]
-    #print (len(first_half), second_half)
-
-
     for i in range(0, len(first_half)):
         if first_half[i] in second_half:
-            #print("matching letter", first_half[i])
-            matching_letters.append(first_half[i])
+            matching_items.append(first_half[i])
             break
 
-print (matching_letters)
-print (priority)
-print(priority.index('A')+1)
-
-sum_of_priorities = 0
-for letter in matching_letters:
-    sum_of_priorities += (priority.index(letter) + 1)
-
-print(sum_of_priorities)
-
-########################
-# second part
-###########
-
-matching_letters2 = []
+# Find matching item in sets of 3
+matching_badges = []
 for i in range(0, len(rucksacks), 3):
     set_of_elfs = rucksacks[i: i+3]
 
     for i in range(0, len(set_of_elfs[0])):
         if set_of_elfs[0][i] in set_of_elfs[1] and set_of_elfs[0][i] in set_of_elfs[2]:
-            matching_letters2.append(set_of_elfs[0][i])
+            matching_badges.append(set_of_elfs[0][i])
             break
 
-print(matching_letters2)
 
-sum_of_priorities2 = 0
-for letter in matching_letters2:
-    sum_of_priorities2 += (priority.index(letter) + 1)
+print(f'Sum of priorities are: {calculate_prio(matching_items)}')
+print(f'Sum of badges are: {calculate_prio(matching_badges)}')
 
-print(sum_of_priorities2)
